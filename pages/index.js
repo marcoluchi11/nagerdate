9a8db9f77ec9558a169c1c81c8f689d56582a276
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-21 22:14:20 -0300

    put the tags and functions in another component to make a more clean index

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,17 @@
-import Header from "@/components/Header";
+import DaysLeft from "@/components/DaysLeft";
 import Loading from "@/components/Loading";
 import { NagerContext } from "@/context/context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
-export default function Home({ data }) {
+export default function Home() {
   const { dates } = useContext(NagerContext);
-  const handleClick = (date) => {
-    const now = new Date();
-    const dateSplit = date.date.split("-");
-    const feriado = new Date(dateSplit[0], dateSplit[1] - 1, dateSplit[2]);
-    // var diff = Math.abs(feriado - now);
-    console.log(feriado.getDay());
-    // const fechafinal = new Date(diff);
-  };
+  const [toggle, setToggle] = useState(false);
   if (dates.length === 0) return <Loading />;
 
   return (
     <>
-      <Header />
-      <section>
-        {dates.map((date, index) => (
-          <div key={index}>
-            <h1>{date.localName}</h1>
-            <p>{date.name}</p>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => handleClick(date)}
-            >
-              Button
-            </button>
-          </div>
-        ))}
+      <section className=" bg-black text-white flex flex-col items-center justify-around h-full">
+        <DaysLeft setToggle={setToggle} toggle={toggle} />
       </section>
     </>
   );
