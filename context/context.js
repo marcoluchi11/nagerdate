ae4b8d4069b307f0bd1ad5da0798bf8548644533
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-29 20:45:30 -0300

    add function to get location and get data with that

--- context/context.js
+++ context/context.js
@@ -8,33 +8,39 @@ const NagerProvider = ({ children }) => {
   const [dates, setDates] = useState([]);
   const [loading, setLoading] = useState(false);
   const [daysLeft, setDaysLeft] = useState(0);
+  const [now, setNow] = useState(new Date());
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async function (position) {
-      setLoading(true);
-      const lat = position.coords.latitude.toFixed(2);
-      const long = position.coords.longitude.toFixed(2);
-      const countryUrl = `http://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${long}&username=marcoluchi11`;
-      const res = await fetch(countryUrl);
-      const result = await res.json();
-      const url = `https://date.nager.at/api/v3/publicholidays/2023/${result.countryCode}`;
-      const res2 = await fetch(url);
-      const result2 = await res2.json();
-      const now = new Date();
-
-      const futureDates = result2.filter((res) => {
-        const { date } = res;
-        const dateFinal = createDateObject(date);
-        if (now < dateFinal) return res;
-      });
-      setDates(futureDates);
-      const feriado = createDateObject(futureDates[0].date);
-      setDaysLeft(dateDiff(now, feriado));
-      setLoading(false);
+      try {
+        setLoading(true);
+        const lat = position.coords.latitude.toFixed(2);
+        const long = position.coords.longitude.toFixed(2);
+        const countryUrl = `http://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${long}&username=marcoluchi11`;
+        const res = await fetch(countryUrl);
+        const result = await res.json();
+        const url = `https://date.nager.at/api/v3/publicholidays/2023/${result.countryCode}`;
+        const res2 = await fetch(url);
+        const result2 = await res2.json();
+        const futureDates = result2.filter((res) => {
+          const { date } = res;
+          const dateFinal = createDateObject(date);
+          if (now < dateFinal) return res;
+        });
+        setDates(futureDates);
+        const feriado = createDateObject(futureDates[0].date);
+        setDaysLeft(dateDiff(now, feriado));
+        setLoading(false);
+      } catch (err) {
+        console.log("There was an error!", err);
+        throw new Error(err);
+      }
       //COMO HACERLO MAS PERFORMANTE
     });
-  }, []);
+  }, [now]);
   return (
-    <NagerContext.Provider value={{ dates, loading, setDaysLeft, daysLeft }}>
+    <NagerContext.Provider
+      value={{ dates, now, loading, setDaysLeft, daysLeft }}
+    >
       {children}
     </NagerContext.Provider>
   );
