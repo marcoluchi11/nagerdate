8ec7b50000e7c6ed3eba1b064770f6f9dfe66ac7
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-29 20:46:21 -0300

    add better design to next feriados

--- components/NextFeriados.jsx
+++ components/NextFeriados.jsx
@@ -1,9 +1,9 @@
 import { NagerContext } from "@/context/context";
-import { changeDateFormat } from "@/helpers";
+import { changeDateFormat, createDateObject, dateDiff } from "@/helpers";
 import { useContext } from "react";
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 const NextFeriados = () => {
-  const { dates } = useContext(NagerContext);
+  const { dates, now } = useContext(NagerContext);
   return (
     <ul className="flex flex-col items-center">
       {dates.map((date, index) => {
@@ -13,12 +13,17 @@ const NextFeriados = () => {
         if (index >= 6) {
           return null;
         } else {
+          const feriado = createDateObject(date.date);
+          const howLong = dateDiff(now, feriado);
           return (
             <li
               key={crypto.randomUUID()}
-              className="p-2 bg-gray-800 my-3 rounded-md font-light "
+              className="p-2 w-full justify-evenly flex md:flex-row items-center bg-gray-800 my-3 rounded-md font-light text-xs md:text-md "
             >
-              {date.localName} {"=>"} {changeDateFormat(date.date)}{" "}
+              <p className="text-sm w-2/4 text-center">{date.localName}</p>
+              <AiOutlineArrowRight className="text-lg" />
+              {changeDateFormat(date.date)}
+              <small className="font-thin">Faltan {howLong} dias</small>
             </li>
           );
         }
