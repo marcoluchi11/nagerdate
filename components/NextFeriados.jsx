ed6361d495007e41d289a3329471fa58956c512d
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-28 23:10:46 -0300

    add show 6 next holiday, and format date with helper function

--- components/NextFeriados.jsx
+++ components/NextFeriados.jsx
@@ -1,20 +1,30 @@
 import { NagerContext } from "@/context/context";
+import { changeDateFormat } from "@/helpers";
 import { useContext } from "react";
 
 const NextFeriados = () => {
   const { dates } = useContext(NagerContext);
-  return dates.map((date, index) => {
-    if (index === 0) {
-      return null;
-    } else {
-      return (
-        <div key={index}>
-          <small className="mr-3">{date.localName}</small>
-          <small className="mr-3">{date.date}</small>
-        </div>
-      );
-    }
-  });
+  return (
+    <ul className="flex flex-col items-center">
+      {dates.map((date, index) => {
+        if (index === 0) {
+          return null;
+        }
+        if (index >= 6) {
+          return null;
+        } else {
+          return (
+            <li
+              key={crypto.randomUUID()}
+              className="p-2 bg-gray-800 my-3 rounded-md font-light "
+            >
+              {date.localName} {"=>"} {changeDateFormat(date.date)}{" "}
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
 };
 
 export default NextFeriados;
