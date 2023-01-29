c0e1d715e9d221dcda354c56a37f0b5a1a8e1f99
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-28 22:58:28 -0300

    add Poppins font

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="../public/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
