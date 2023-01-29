f1ac279fd3adbe6074211014b4688f18f63ecdf7
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-29 20:43:52 -0300

    add proximos 5 feriados

--- components/ButtonNextFeriados.jsx
+++ components/ButtonNextFeriados.jsx
@@ -4,7 +4,7 @@ const ButtonNextFeriados = ({ setToggle, toggle }) => {
       className="inline-block px-6 py-2.5 bg-gray-700 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out mt-8"
       onClick={() => setToggle(!toggle)}
     >
-      {toggle ? "X Cerrar" : "Ver Proximos 3 Feriados"}
+      {toggle ? "X Cerrar" : "Ver Proximos 5 Feriados"}
     </button>
   );
 };
