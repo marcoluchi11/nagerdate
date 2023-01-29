6d599c5f02762d7267988d73c525f1dd7f906c31
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-28 23:05:12 -0300

    add layout and head to index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
 import DaysLeft from "@/components/DaysLeft";
+import Layout from "@/components/Layout";
 import Loading from "@/components/Loading";
 import { NagerContext } from "@/context/context";
+import Head from "next/head";
 import { useContext, useState } from "react";
 
 export default function Home() {
   const { dates } = useContext(NagerContext);
   const [toggle, setToggle] = useState(false);
-  if (dates.length === 0) return <Loading />;
+  if (dates.length === 0)
+    return (
+      <section className="h-screen flex flex-col">
+        <Loading />
+      </section>
+    );
 
   return (
     <>
-      <section className=" bg-black text-white flex flex-col items-center justify-around h-full">
-        <DaysLeft setToggle={setToggle} toggle={toggle} />
-      </section>
+      <Head>
+        <link rel="icon" href="logo.png" />
+        <title>Feriado</title>
+      </Head>
+      <Layout>
+        <section className=" bg-black text-white flex flex-col items-center justify-around h-full">
+          <DaysLeft setToggle={setToggle} toggle={toggle} />
+        </section>
+      </Layout>
     </>
   );
 }
