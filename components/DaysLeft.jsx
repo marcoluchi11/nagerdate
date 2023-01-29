325c4881f338cf2f262f32c508e67f148d4f4a59
marcoluchi11 <marcoaluchi@hotmail.com>
2023-01-29 20:44:41 -0300

    add icon to show next feriado

--- components/DaysLeft.jsx
+++ components/DaysLeft.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import ButtonNextFeriados from "./ButtonNextFeriados";
 import HeaderDays from "./HeaderDays";
 import NextFeriados from "./NextFeriados";
-
+import { AiOutlineArrowRight } from "react-icons/ai";
 const DaysLeft = ({ setToggle, toggle }) => {
   const { daysLeft, dates } = useContext(NagerContext);
   return (
@@ -12,8 +12,10 @@ const DaysLeft = ({ setToggle, toggle }) => {
       <div className="flex flex-col md:w-screen items-center h-full justify-center md:justify-center">
         <HeaderDays daysLeft={daysLeft} />
         <div className="flex flex-col items-center">
-          <small className="font-thin">
-            {dates[0].localName} {"=>"} {changeDateFormat(dates[0].date)}
+          <small className="font-thin flex items-center">
+            {dates[0].localName}{" "}
+            <AiOutlineArrowRight className="text-md mx-2" />{" "}
+            {changeDateFormat(dates[0].date)}
           </small>
           <ButtonNextFeriados setToggle={setToggle} toggle={toggle} />
         </div>
